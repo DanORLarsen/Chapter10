f7061bc8eaf2ec2ce8690db89b382897bc00323c
DanOliverRunoLarsen <42993978+DanOliverRunoLarsen@users.noreply.github.com>
2018-10-29 16:53:30 +0100

    Done with Time class

--- src/Time.java
+++ src/Time.java
@@ -3,17 +3,44 @@ public class Time {
     long minute;
     long second;
 
+    public long getHour() {
+        return hour;
+    }
+
+    public long getMinute() {
+        return minute;
+    }
+
+    public long getSecond() {
+        return second;
+    }
+
     public Time()
-    {long mili = System.currentTimeMillis();
-    hour = mili/3600000;
-    minute = (mili%360000)/60000;
-    second = ((mili%360000)%6000)/1000;
+    {
+        long mili = System.currentTimeMillis();
+        this.hour = mili/3600000;
+        this.minute = (mili%360000)/60000;
+        this.second = ((mili%360000)%6000)/1000;
     }
 
     public Time(long elapsedMiliseconds)
-    {hour = elapsedMiliseconds/3600000;
-        minute = (elapsedMiliseconds%360000)/60000;
-        second = ((elapsedMiliseconds%360000)%6000)/1000;
+    {
+        this.hour = elapsedMiliseconds/3600000;
+        this.minute = (elapsedMiliseconds%360000)/60000;
+        this.second = ((elapsedMiliseconds%360000)%6000)/1000;
+    }
 
+    public Time(long hours, long minutes, long seconds)
+    {
+        this.hour = hours;
+        this.minute = minutes;
+        this.second = seconds;
+    }
+
+    public void setTime(long elapsedTime)
+    {
+        this.hour = elapsedTime/3600000;
+        this.minute = (elapsedTime%360000)/60000;
+        this.second = ((elapsedTime%360000)%6000)/1000;
     }
 }
